b24fa9da318a57eb725c9d557d98527af204a1fc
boy-hack <34109680@qq.com>
2017-07-26 17:07:58 +0800

    sql文件

--- theme/about.php
+++ theme/about.php
@@ -6,8 +6,10 @@
     <div class="page-header">
         <h1>About <small>w8scan 扫描器</small></h1>
     </div>
-    哪有那么多时间BB？能BB不如多写代码</br>
-    <a href="https://yesfree.pw/" target="_blank">我的博客</a>欢迎光临
+    本扫描器代码完全开源</br>
+    仅提供个人以及非盈利性安全团队使用</br>
+    切勿使用本扫描器用于非法用途！否则后果自负！</br>
+    <a href="https://blog.hacking8.com/" target="_blank">我的博客</a>欢迎光临
     </div>
 </div>
 </div>
